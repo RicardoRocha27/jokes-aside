6bf9f3c41fa1f27b2157d25313c8eb8050919953
RicardoRocha27 <133434333+RicardoRocha27@users.noreply.github.com>
2023-08-28 16:10:37 +0100

    Home page hero implemented

---
+++ app/(main)/(routes)/home/components/hero.tsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <div className="flex flex-col gap-y-5 mt-10 mb-20 text-center">
+      <h1 className="font-bold text-6xl">
+        Laugh & Share
+        <br />
+        <span className="bg-gradient-to-r from-pink-800 via-cyan-700 to-pink-800 bg-clip-text text-transparent">
+          Homemade Wisecracks
+        </span>
+      </h1>
+      <p className="">
+        Jokes Aside is an open-source joke sharing hub where everyone can share
+        their uncle's last christmas jokes and more, of course.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
--- app/(main)/(routes)/home/page.tsx
+++ app/(main)/(routes)/home/page.tsx
@@ -1,8 +1,11 @@
 import PostList from "@/components/post-list";
 
+import Hero from "./components/hero";
+
 const HomePage = () => {
   return (
     <>
+      <Hero />
       <PostList />
     </>
   );
